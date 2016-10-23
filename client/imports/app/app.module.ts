87f1ade5a1b3d7719884f82b3523bfece4d02c6a
Rokas Leskevicius <rokas.l@ufl.edu>
2016-10-23 19:33:10 -0400

    working on desktop

--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AccountsModule } from 'angular2-meteor-accounts-ui';
 
 import { AppComponent } from './app.component';
+import { routes, ROUTES_PROVIDERS } from './app.routes';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    AccountsModule
   ],
   declarations: [
     AppComponent
+  ],
+  providers: [
+    // ...ROUTES_PROVIDERS
   ],
   bootstrap: [
     AppComponent
