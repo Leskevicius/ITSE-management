f6b0df5453e2ff4ed6f350d1b4a49dab9c145eb8
leskevicius <rokaslopas@gmail.com>
2016-10-24 18:38:59 -0400

    .

--- client/imports/app/app.routes.ts
+++ client/imports/app/app.routes.ts
@@ -6,17 +6,17 @@ import { Meteor } from 'meteor/meteor';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './auth/login.component';
 import { LandingPageComponent } from './landing/landing-page.component';
-// import { SignupComponent } from './auth/signup.component';
-// import { RecoverComponent } from "./auth/recover.component";
+import { SignupComponent } from './auth/signup.component';
+import { RecoverComponent } from "./auth/recover.component";
 import { AppComponent } from './app.component';
 
 export const routes: Route[] = [
   // { path: 'asd', component: AppComponent },
   // { path: 'party/:partyId', component: PartyDetailsComponent, canActivate: ['canActivateForLoggedIn']},
   { path: '', component: LandingPageComponent },
-  { path: 'login', component: LoginComponent }
-  // { path: 'signup', component: SignupComponent },
-  // { path: 'recover', component: RecoverComponent }
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'recover', component: RecoverComponent }
 ];
 
 export const ROUTES_PROVIDERS = [{
